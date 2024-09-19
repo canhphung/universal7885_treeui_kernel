ff546bffaab4989452a2acb9661c5ef8775151ea
Canh Phung <gcon5827@gmail.com>
2024-09-19 10:45:46 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7885-a40_defconfig
+make exynos7885-a7y18lte_defconfig
 make -j64
